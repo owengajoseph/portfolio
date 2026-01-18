b4637057f32d369f584147588ab573c0aa0bd3da
owenga <owengajoseph54@gmail.com>
2026-01-18 13:26:53 +0300

    add teser to minify code

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,7 @@ export default defineConfig({
   },
   build: {
     target: 'es2020',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: true,
     rollupOptions: {
       output: {
         manualChunks: {
